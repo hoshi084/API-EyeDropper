ded80fe519c049a20124987c664ad5d9bb5b0a61
mv63 <grandinmatheo59@gmail.com>
2026-04-07 10:42:34 +0200

    commit3

--- API_exo/app.js
+++ API_exo/app.js
@@ -1,17 +1,19 @@
-// La fonction magique pour ta présentation
-async function pickColor() {
-  if (!window.EyeDropper) {
-    alert("Ton navigateur ne supporte pas cette API !");
-    return;
-  }
+const btn = document.getElementById('startBtn');
+const display = document.getElementById('colorDisplay');
 
-  const eyeDropper = new EyeDropper();
-  
-  try {
-    const result = await eyeDropper.open();
-    document.body.style.backgroundColor = result.sRGBHex;
-    console.log("Couleur choisie :", result.sRGBHex);
-  } catch (err) {
-    console.log("erreur");
-  }
-}
+btn.addEventListener('click', async () => {
+    if (!window.EyeDropper) {
+        alert("Navigateur non supporté");
+        return;
+    }
+
+    const dropper = new EyeDropper();
+
+    try {
+        const result = await dropper.open();
+        document.body.style.backgroundColor = result.sRGBHex;
+        display.textContent = result.sRGBHex;
+    } catch (error) {
+        console.log("Annulé");
+    }
+});
